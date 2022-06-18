b69203de43707b9ced761e40556356a9ac13b8df
ashrhmn <ashrhmn99@gmail.com>
2022-06-19 05:11:10 +0600

    Add contact list

--- PhoneBook/main.py
+++ PhoneBook/main.py
@@ -3,6 +3,17 @@ from os.path import exists
 CONTACTS = []
 
 
+def invalid_error():
+    print("Invalid Input")
+
+
+def is_confirmed(text):
+    decision = input(f"{text} (y,n) : ")
+    if decision.lower() == "y":
+        return True
+    return False
+
+
 def init():
     if exists("PhoneBook/data.txt"):
         data_file = open("PhoneBook/data.txt", "r")
@@ -30,14 +41,85 @@ def save_file():
     data_file.close()
 
 
+def take_contact_inputs():
+    name = input("Name : ")
+    address = input("Address : ")
+    email = input("Email : ")
+    phones = input("Phone (Comma Separated) : ").split(",")
+    return {"name": name, "address": address, "email": email, "phones": phones}
+
+
+def add_contact():
+    print("New Contact :")
+    CONTACTS.append(take_contact_inputs())
+    print_contacts()
+    if is_confirmed("Add again ?"):
+        add_contact()
+
+
+def update_contact():
+    print_contacts()
+    option = int(input("Enter a number to update : "))
+    if option <= len(CONTACTS):
+        CONTACTS[option - 1] = take_contact_inputs()
+        print_contacts()
+        if is_confirmed("Update again?"):
+            remove_contact()
+    else:
+        print("Invalid input")
+
+
+def print_contacts():
+    index = 0
+    for contact in CONTACTS:
+        index += 1
+        print("\n", index, " .")
+        print("Name : ", contact["name"])
+        print("Address : ", contact["address"])
+        print("Email : ", contact["email"])
+        print("Phones : ", ", ".join(contact["phones"]), "\n")
+
+
+def remove_contact():
+    print_contacts()
+    option = int(input("Enter a number to remove : "))
+    if option <= len(CONTACTS):
+        del CONTACTS[option - 1]
+        print_contacts()
+        if is_confirmed("Delete again?"):
+            remove_contact()
+    else:
+        print("Invalid input")
+
+
 def main():
-    print(
+    init()
+    while True:
+        print(
+            """
+        Welcome to Contact List
+
+        1. View Contacts
+        2. Add New Contact
+        3. Update Contact
+        4. Remove Contact
+        0. Exit
         """
-    Welcome to PhoneBook
-    """
-    )
+        )
+        option = int(input("Enter an option : "))
+        if option == 0:
+            break
+        menus = {
+            1: print_contacts,
+            2: add_contact,
+            3: update_contact,
+            4: remove_contact,
+        }
+        if menus.keys().__contains__(option):
+            menus[option]()
+        else:
+            print("Invalid Option")
+    save_file()
 
 
-init()
-print(CONTACTS)
-save_file()
+main()
