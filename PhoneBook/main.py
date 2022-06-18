5b618a74d606177918de0019803054c3ea20a3f1
ashrhmn <ashrhmn99@gmail.com>
2022-06-19 05:28:44 +0600

    Add search feature to contact list

--- PhoneBook/main.py
+++ PhoneBook/main.py
@@ -52,26 +52,29 @@ def take_contact_inputs():
 def add_contact():
     print("New Contact :")
     CONTACTS.append(take_contact_inputs())
-    print_contacts()
+    print_all_contacts()
     if is_confirmed("Add again ?"):
         add_contact()
 
 
 def update_contact():
-    print_contacts()
+    print_all_contacts()
     option = int(input("Enter a number to update : "))
     if option <= len(CONTACTS):
         CONTACTS[option - 1] = take_contact_inputs()
-        print_contacts()
+        print_all_contacts()
         if is_confirmed("Update again?"):
             remove_contact()
     else:
         print("Invalid input")
 
 
-def print_contacts():
+def print_contacts(contacts):
+    if len(contacts) == 0:
+        print("\n\nNo contact found\n\n")
+        return
     index = 0
-    for contact in CONTACTS:
+    for contact in contacts:
         index += 1
         print("\n", index, " .")
         print("Name : ", contact["name"])
@@ -80,12 +83,37 @@ def print_contacts():
         print("Phones : ", ", ".join(contact["phones"]), "\n")
 
 
+def print_all_contacts():
+    print_contacts(CONTACTS)
+
+
+def search_contact(query):
+    results = []
+    for item in CONTACTS:
+        if (
+            item["name"].lower().__contains__(query.lower())
+            or item["address"].lower().__contains__(query.lower())
+            or item["email"].lower().__contains__(query.lower())
+        ):
+            results.append(item)
+        else:
+            for phone in item["phones"]:
+                if phone.lower().__contains__(query.lower()):
+                    results.append(item)
+    return results
+
+
+def show_search_result():
+    query = input("Search for contact : ")
+    print_contacts(search_contact(query))
+
+
 def remove_contact():
-    print_contacts()
+    print_all_contacts()
     option = int(input("Enter a number to remove : "))
     if option <= len(CONTACTS):
         del CONTACTS[option - 1]
-        print_contacts()
+        print_all_contacts()
         if is_confirmed("Delete again?"):
             remove_contact()
     else:
@@ -103,6 +131,7 @@ def main():
         2. Add New Contact
         3. Update Contact
         4. Remove Contact
+        5. Search Contact
         0. Exit
         """
         )
@@ -110,10 +139,11 @@ def main():
         if option == 0:
             break
         menus = {
-            1: print_contacts,
+            1: print_all_contacts,
             2: add_contact,
             3: update_contact,
             4: remove_contact,
+            5: show_search_result,
         }
         if menus.keys().__contains__(option):
             menus[option]()
